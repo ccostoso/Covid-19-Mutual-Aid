e14fa66424bc00fd6f8269d8b3e690e6601e9315
ccostoso <carlos.costoso@outlook.com>
2020-03-22 13:27:19 -0400

    Added react-router-dom to client/package.json

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const apartmentRoutes = require("./apartment");
+const communityRoutes = require("./community");
 const userRoutes = require("./user");
 
 // Book routes
-router.use("/apartment", apartmentRoutes);
+router.use("/community", communityRoutes);
 router.use("/user", userRoutes);
 
 module.exports = router;
