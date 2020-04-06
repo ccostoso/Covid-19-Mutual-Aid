1faef187d21bea7813dc842b3570afc97564d51b
ccostoso <carlos.costoso@outlook.com>
2020-04-06 13:28:09 -0400

    Installed bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,11 +16,69 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    console.log(req.body);
-    db.User
-      .create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+    console.log("req.body", req.body);
+
+    const { body } = req;
+    const { password } = body;
+    let { email } = body;
+    
+    if (!email) {
+      console.log("!!!!!!")
+      return res.send({
+        success: false,
+        message: 'Error: Email cannot be blank.'
+      });
+    }
+
+    if (!password) {
+      return res.send({
+        success: false,
+        message: 'Error: Password cannot be blank.'
+      });
+    }
+
+    email = email.toLowerCase();
+    email = email.trim();
+    // Steps:
+    // 1. Verify email doesn't exist
+    // 2. Save
+    db.User.find({
+      email: email
+    }, (err, previousUsers) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: Server error'
+        });
+      } else if (previousUsers.length > 0) {
+        return res.send({
+          success: false,
+          message: 'Error: Account already exist.'
+        });
+      }
+      // Save the new user
+      const newUser = new User();
+      newUser.email = email;
+      newUser.password = newUser.generateHash(password);
+      newUser.save((err, user) => {
+        if (err) {
+          return res.send({
+            success: false,
+            message: 'Error: Server error'
+          });
+        }
+        return res.send({
+          success: true,
+          message: 'Signed up'
+        });
+      });
+    });
+
+
+    // db.User
+    //   .create(req.body)
+    //   .then(dbModel => res.json(dbModel))
+    //   .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
     db.User
