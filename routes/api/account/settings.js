d0b58ff5da2ba9874d6fa39135b966cc48fa47ad
annammattis <54290779+annammattis@users.noreply.github.com>
2020-04-13 14:15:51 -0400

    added settings.js to routes

---
+++ routes/api/account/settings.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+} from 'react-router-dom'
+import Settings from './settings'
+import { fetchSettings } from './api'
+
+const routes = [
+    {
+        path: '/settings',
+        component: Settings,
+        fetchInitialData: (id) => fetchSettings(id),
+    }
+]
+//SETTING ROUTES WILL GO IN HERE
