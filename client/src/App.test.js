c589775677fceda9e38a84039ec331937a34e6b6
Anna-Marjorie Mattis <annammattis@gmail.com>
2020-04-16 18:57:22 -0400

    Theme font2 (#15)
    * updated versions
    
    * account verify route
    
    * updated register signup
    
    * updates to routes, pages and utils
    
    * updated verify.js and userController.js
    
    * added settings under pages
    
    * testing input for settings
    
    * testing settings
    
    * added more settings to js file
    
    * updated register js
    
    * added active status
    
    * added themes option
    
    * added css to settings
    
    * tested in app.js
    
    * added settings.js to routes
    
    * added props to components file
    
    * added changes to client folder
    
    * added skeleton for settings
    
    * edited settings
    
    * removed old components
    
    * deleted some settings
    
    * edited fontSize
    
    * added settings in root folder
    
    * testing out switch-lang
    
    * changed thai to french
    
    * adjusting language rendering
    
    * testing languages
    
    * adding new package
    
    * adding files for settings
    
    * updating front end settings
    
    * finishing language feature
    
    * changing settings
    
    * rendered functionality for language buttons
    
    * updated hard text
    
    * finished first draft of languages
    
    * updated app.js
    
    * started theme setting
    
    * added dark and light theme brightness
    
    * added brightness to settings
    
    * added brightness capabilities to settings
    
    * updated settings index
    
    * merged edit
    
    * added font code
    
    * added new settings attribute folder and file
    
    * populated new js file
    
    * fixed light and dark theme

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -8,79 +8,114 @@
 //   expect(linkElement).toBeInTheDocument();
 // });
 
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Nav } from "./components/UniversalComponents/Nav";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Home from "./pages/Home";
-import Community from "./pages/Community";
-import Settings from "./pages/Settings";
 
-import { I18nProvider, LOCALES } from './i18n';
-// import translate from './i18n/translate';
+// import React, { Component } from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { Nav } from "./components/UniversalComponents/Nav";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import Home from "./pages/Home";
+// import Community from "./pages/Community";
+// import Settings from "./pages/Settings";
+// import { I18nProvider, LOCALES } from './i18n';
+// // import translate from './i18n/translate';
+// import storage from 'local-storage-fallback';
+// import useTheme from './i18n/themeFont/useTheme'
+// import { ThemeProvider, createGlobalStyle } from 'styled-components';
+// import './App.css';
+// import style from 'styled-theming';
 
-import { ThemeProvider, createGlobalStyle } from 'styled-components';
-import './App.css';
 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      locale: LOCALES.FRENCH,
-      theme: { mode:'light' }
-    }
-    this.handleSetLanguage=this.handleSetLanguage.bind(this)
-  }
- // const [locale, setLocale] = useState(LOCALES.ENGLISH);
-//  const [theme, setTheme] = useState({ mode: 'dark' })
+// const FontSize = createFontSize = style('textZoom', {
+//   normal: '1em',
+//   magnify: '1.2em'
+// })
 
- const GlobalStyle = createGlobalStyle `
- body {
-   background-color: ${props => props.theme.mode === 'dark' ? '#111' : '#EEE'};
-   color: ${props => props.theme.mode === 'dark' ? '#EEE' : '#111'};
- }
- `;
+// const GlobalStyle = createGlobalStyle `
+// body {
+//   background-color: ${props => props.theme.mode === 'dark' ? '#191515' : '#EEE'};
+//   color: ${props => props.theme.mode === 'dark' ? '#EEE' : '#111'};
+//   font-size: ${getFontSize}
+// }
+// nav {
+//   background-color: ${props => props.theme.mode === 'dark' ? '#730808' : '#e00a0a'};
+// }
+// `;
 
-  //   componentDidMount() {
-//     // Do this two lines only when setting up the application
-//     setTranslations({ en, fr });
-//     setDefaultLanguage('en');
-//     /* switch lang code */
+
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       locale: LOCALES.FRENCH,
+//       theme: { mode:'light' }
+//       textZoom : {'normal'}
+//     }
+//     this.handleSetLanguage=this.handleSetLanguage.bind(this)
+//     this.handleSetBrightness=this.handleSetBrightness.bind(this)
 //   }
+//  // const [locale, setLocale] = useState(LOCALES.ENGLISH);
+// //  const [theme, setTheme] = useState({ mode: 'dark' })
 
-  handleSetLanguage = event => {
-    event.preventDefault()
-    console.log(event.target.value);
 
-    this.setState({
-      locale: event.target.value
-    })
-  };
-  
- 
 
-  render() {
-    return (
-      <ThemeProvider theme={this.state.theme}>
-        <>
-          <GlobalStyle />
-          <I18nProvider locale={this.state.locale}>
-            <Router>
-              <Nav />
-              <Route exact path="/" component={Login} />
-              <Route exact path="/register" component={Register} />
-              <Route path="/home" component={Home} />
-              <Route path="/community/:id" component={Community} />
-              <Route path="/settings" component={() => <Settings setLanguage={this.handleSetLanguage} />} />
-            </Router>
-          </I18nProvider>
-          <button>Brightness</button>
-        </>
-      </ThemeProvider>
-    )
-  }
-}
+//   //   componentDidMount() {
+// //     // Do this two lines only when setting up the application
+// //     setTranslations({ en, fr });
+// //     setDefaultLanguage('en');
+// //     /* switch lang code */
+// //   }
 
-export default App;
+//   handleSetLanguage = event => {
+//     event.preventDefault()
+//     console.log(event.target.value);
+
+//     this.setState({
+//       locale: event.target.value
+//     })
+//   };
+
+//   handleSetBrightness = event => {
+//     event.preventDefault()
+//     console.log(event.target.value);
+
+//     this.setState({
+//       theme: { mode: event.target.value }
+//     })
+//   };
+
+//   handleSetFontSize = event => {
+//     event.preventDefault()
+//       console.log(event.target.value);
+
+//       this.setState({
+//         textZoom: { 'normal' };
+//       })
+//   };
+
+
+
+//   render() {
+//     return (
+//       <I18nProvider locale={this.state.locale}>
+//       <ThemeProvider theme={theme}> 
+//         <>
+//           <GlobalStyle />
+//             <Router>
+//               <Nav />
+//               <Route exact path="/" component={Login} />
+//               <Route exact path="/register" component={Register} />
+//               <Route path="/home" component={Home} />
+//               <Route path="/community/:id" component={Community} />
+//               <Route path="/settings" component={() => <Settings setLanguage={this.handleSetLanguage} setBrightness={this.handleSetBrightness} />} />
+//             </Router>
+//         </>
+//       </ThemeProvider>
+//       </I18nProvider>
+//     )
+//   }
+// }
+
+// export default App;
--- client/src/i18n/themeFont/useTheme.js
+++ client/src/i18n/themeFont/useTheme.js
@@ -1,27 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import storage from 'local-storage-fallback';
 
-export default function useTheme(
-    defaultTheme={ mode: 'light', textZoom: 'normal' }) 
-    {
-    const[theme, _setTheme] = useState(getInitialTheme)
+// import React, { useState, useEffect } from 'react';
+// import storage from 'local-storage-fallback';
 
-    function getInitialTheme() {
-        const savedTheme = storage.getItem('theme')
-        return savedTheme
-            ? JSON.parse(savedTheme)
-            : defaultTheme;
-    }
+// export default function useTheme(
+//     defaultTheme={ mode: 'light', textZoom: 'normal' }) 
+//     {
+//     const[theme, _setTheme] = useState(getInitialTheme)
 
-    useEffect(
-        () => {
-            storage.setItem('theme', JSON.stringify(theme));
-        },
-        [theme]
-    );
-    return {
-        ...theme,
-        setTheme; ({setTheme, ...theme}) = _setTheme(theme)
-    }
-}
+//     function getInitialTheme() {
+//         const savedTheme = storage.getItem('theme')
+//         return savedTheme
+//             ? JSON.parse(savedTheme)
+//             : defaultTheme;
+//     }
+
+//     useEffect(
+//         () => {
+//             storage.setItem('theme', JSON.stringify(theme));
+//         },
+//         [theme]
+//     );
+//     return {
+//         ...theme,
+//         setTheme: ({setTheme, ...theme}) = _setTheme(theme)
+//     }
+// }
+
 
