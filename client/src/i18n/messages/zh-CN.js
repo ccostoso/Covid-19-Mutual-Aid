ace73160f11a485da0880f74fe88e3da5c5d000c
szeeepeiii <55262650+szeeepeiii@users.noreply.github.com>
2020-04-16 21:50:24 -0400

    Kristen (#19)
    * add chinese language
    
    * Chinese language part 1
    
    * chinese part 1
    
    Co-authored-by: Anna-Marjorie Mattis <annammattis@gmail.com>

--- client/src/i18n/messages/zh-CN.js
+++ client/src/i18n/messages/zh-CN.js
@@ -2,7 +2,30 @@ import { LOCALES } from '../locales';
 
 export default {
     [LOCALES.CHINESE]: {
-       
-        
+        'Settings':'设定',
+        'Languages': '语言',
+        'Font Size': '字体大小',
+        'Brightness' : '亮度',
+        'Save': '保存',
+        "We won't share your personal information with anyone.": "我们不会与任何人分享您的个人信息.",
+        'English': '英文',
+        'French': '法文',
+        'Register': "登记",
+        'Display Name': '显示名称',
+        'Email': '电子邮件',
+        'Password': '密码',
+        'Date of Birth': '出生日期',
+        'Age': '年龄',
+        'News': '新闻',
+        "News and Alerts": "新闻和警报",
+        "Message Board": "留言板",
+        "Alerts": "警报",
+        "My Communities": "我的社区",
+        "Communities": "社区",
+        "Messages": "信息",
+        "Login": "登入",
+        "New user? Create an account": "新用户? 创建账户",
+        "About": "关于",
+        "Account": "账户",
     }
 }
