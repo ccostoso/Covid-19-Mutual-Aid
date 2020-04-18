93e9ff984de05a3d22468d1480b46373d9a7f4ee
annammattis <54290779+annammattis@users.noreply.github.com>
2020-04-18 13:48:55 -0400

    provided context in i18n

--- client/src/i18n/translate.js
+++ client/src/i18n/translate.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
+//"FormattedMessage" This component uses the formatMessage API and has props that correspond to a Message Descriptor.
 const translate = (id, value={}) => <FormattedMessage id={id} value={{...value}}/>;
 
 export default translate;
