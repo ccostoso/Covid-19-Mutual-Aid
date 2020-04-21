32d2301fce217c4b09a1147260e997bc678929e9
annammattis <54290779+annammattis@users.noreply.github.com>
2020-04-21 16:06:54 -0400

    adding profile picture capabilities

--- routes/api/account/index.js
+++ routes/api/account/index.js
@@ -5,12 +5,14 @@ const logInRoutes = require("./login");
 const logOutRoutes = require("./logout");
 const registerRoutes = require("./register");
 const userRoutes = require("./user");
+const putAvatarRoutes =  require("./avatar");
 
 // // USE PASSPORT API ROUTES
 router.use("/login", logInRoutes);
 router.use("/logout", logOutRoutes);
 router.use("/register", registerRoutes);
 router.use("/user", userRoutes);
+router.use('/avatar', putAvatarRoutes);
 
 // Export to ../index.js
 module.exports = router;
