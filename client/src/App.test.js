48669bdf12e516675e8cb8f2e96a1f2de70f299d
Anna-Marjorie Mattis <annammattis@gmail.com>
2020-04-21 10:23:29 -0400

    Theme font2 (#38)
    * updated versions
    
    * account verify route
    
    * updated register signup
    
    * updates to routes, pages and utils
    
    * updated verify.js and userController.js
    
    * added settings under pages
    
    * testing input for settings
    
    * testing settings
    
    * added more settings to js file
    
    * updated register js
    
    * added active status
    
    * added themes option
    
    * added css to settings
    
    * tested in app.js
    
    * added settings.js to routes
    
    * added props to components file
    
    * added changes to client folder
    
    * added skeleton for settings
    
    * edited settings
    
    * removed old components
    
    * deleted some settings
    
    * edited fontSize
    
    * added settings in root folder
    
    * testing out switch-lang
    
    * changed thai to french
    
    * adjusting language rendering
    
    * testing languages
    
    * adding new package
    
    * adding files for settings
    
    * updating front end settings
    
    * finishing language feature
    
    * changing settings
    
    * rendered functionality for language buttons
    
    * updated hard text
    
    * finished first draft of languages
    
    * updated app.js
    
    * started theme setting
    
    * added dark and light theme brightness
    
    * added brightness to settings
    
    * added brightness capabilities to settings
    
    * updated settings index
    
    * merged edit
    
    * added font code
    
    * added new settings attribute folder and file
    
    * populated new js file
    
    * fixed light and dark theme
    
    * testing font size
    
    * added font code
    
    * added font code
    
    * added to languages
    
    * merged
    
    * beta testing
    
    * partially rendered the mode and size for theme
    
    * testing size
    
    * finished fontSize setting
    
    * updates
    
    * updated settings renderings
    
    * updates
    
    * deleted useTheme.js
    
    * deleted settings.js on root
    
    * updated default language
    
    * added explanation throughout app.js
    
    * provided context in i18n
    
    * updated resgister

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,72 +1,144 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, useParams, useRouteMatch } from "react-router-dom";
+import { Nav } from "./components/UniversalComponents/Nav";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Community from "./pages/Community";
+import Settings from "./pages/Settings";
+import { I18nProvider, LOCALES } from './i18n';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import './App.css';
+import API from "./utils/API";
+
+// A helper function to generate a special StyledComponent that handles global styles.
+// Returns a StyledComponent that does not accept children. 
+// Place it at the top of your React tree and the global styles will be injected when the component is "rendered". 
+const GlobalStyle = createGlobalStyle`
+body {
+  background-color: ${props => props.theme.mode === 'dark' ? '#191515' : '#EEE'};
+  color: ${props => props.theme.mode === 'dark' ? '#EEE' : '#191515'};
+  font-size: ${props => props.theme.size === 'normal' ? '1em' : '1.2em'};
+}
+nav {
+  background-color: ${props => props.theme.mode === 'dark' ? '#730808' : '#e00a0a'};
+}
+`;
+
 class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        locale: LOCALES.FRENCH,
-        theme: { 
-          mode: 'light',
-          size: 'normal'
-        }
-      }
-      this.handleSetLanguage=this.handleSetLanguage.bind(this)
-      this.handleSetBrightness=this.handleSetBrightness.bind(this)
-      this.handleSetFontSize=this.handleSetFontSize.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      locale: LOCALES.ENGLISH,
+      theme: {
+        mode: 'light',
+        size: 'normal'
+      },
+      user: {
+        email: '',
+        password: '',
+        jsonMessage: '',
+        userId: "",
+      },
+      isLoggedIn: false,
     }
-  
-  
-    //   componentDidMount() {
-    //   // Do this two lines only when setting up the application
-    // }
-  
-    handleSetLanguage = event => {
-      event.preventDefault()
-      console.log(event.target.value);
-        
-      this.setState({
-        locale: event.target.value
-      })
+  }
+
+  //triggers the selection of languages to be rendered throughout the app
+  handleSetLanguage = event => {
+    event.preventDefault()
+
+    this.setState({
+      locale: event.target.value
+    })
+  };
+  //triggers the dark and light mode theme to the app
+  handleSetBrightness = event => {
+    event.preventDefault()
+    const newMode = event.target.value
+
+    this.setState({
+      theme: {
+        mode: newMode === "light" ? "light" : "dark"
+      }
+    })
+  };
+
+  //triggers whether the font throughout the app will be magnified or not
+  handleSetFontSize = event => {
+    event.preventDefault()
+    const newSize = event.target.value
+
+    this.setState({
+      theme: {
+        size: newSize === "normal" ? "normal" : "magnify"
+      }
+    })
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+
+    this.setState({
+      user: {
+        ...this.state.user,
+        [name]: value
+      }
+    })
+  }
+
+  handleClick = async e => {
+    e.preventDefault();
+
+    const user = {
+      email: this.state.user.email,
+      password: this.state.user.password
     };
-  
-    handleSetBrightness = event => {
-    //   event.preventDefault()
-      console.log(event.target);
-      var newState = this.state.theme;
-      newState.mode = event.target.value
-      
-      this.setState({
-        theme: newState
-      })
-    };
-  
-    handleSetFontSize = event => {
-    //   event.preventDefault()
-      console.log(event.target);
-      var newState = this.state.theme;
-      newState.size = event.target.value
-      this.setState({
-        theme: newState
-      })
-    };
-  
-  
-  
-    render() {
-      return (
-        <I18nProvider locale={this.state.locale}>
+
+    const response = await API.signIn(user)
+
+    response.status === 200 && this.setState({
+      user: {
+        ...this.state.user,
+        userId: response.data._id,
+      }
+    })
+  }
+
+  //"ThemeProvider" A helper component for theming. Injects the theme into all styled components 
+  // anywhere beneath it in the component tree, via the context API.
+  render() {
+    return (
+      <I18nProvider locale={this.state.locale}>
         <ThemeProvider theme={this.state.theme}>
           <>
             <GlobalStyle />
-              <Router>
-                <Nav />
-                <Route exact path="/" component={Login} />
-                <Route exact path="/register" component={Register} />
-                <Route path="/home" component={Home} />
-                <Route path="/community/:id" component={Community} />
-                <Route path="/settings" component={() => <Settings setLanguage={this.handleSetLanguage} setBrightness={this.handleSetBrightness} setFontSize={this.handleSetFontSize} />} />
-              </Router>
+            <Router>
+              <Nav />
+              <Route exact path="/" render={() => {
+                  return <Login
+                    user={this.state.user}
+                    handleChange={this.handleChange}
+                    handleClick={this.handleClick}
+                  />
+                }
+              } />
+              <Route exact path="/register" component={Register} />
+              <Route path="/home/:id" render={() => {
+                  return <Home 
+                    user={this.state.user}
+                  />
+                }} 
+              />
+              <Route path="/community/:id" component={Community} />
+              <Route path="/settings" component={() => <Settings setLanguage={this.handleSetLanguage} setBrightness={this.handleSetBrightness} setFontSize={this.handleSetFontSize} />} />
+            </Router>
           </>
         </ThemeProvider>
-        </I18nProvider>
-      )
-    }
+      </I18nProvider>
+    )
   }
+}
+
+export default App;
+
