a4d59b64c2e9f9a9879535d209fe9b4aab8c2d1d
ccostoso <carlos.costoso@outlook.com>
2020-04-26 12:56:26 -0400

    Switching branches

--- client/src/components/HomeComponents/UserPanel/index.js
+++ client/src/components/HomeComponents/UserPanel/index.js
@@ -3,18 +3,22 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Col } from "../../UniversalComponents/Grid";
 import { UserPanelNav } from "../UserPanelNav";
 import UserCommunities from "../UserCommunities";
+import UserFindCommunities from "../UserFindCommunities"
 
 export const UserPanel = props => {
     console.log("USERPANEL PROPS", props)
     return (
         <Col size="md-10" className="border-left">
-            {/* <Router> */}
-            <UserPanelNav {...props} />
-            {/* <Route path={`/home/${props.user._id}`} render={() => { */}
-            <UserCommunities {...props} />
-            {/* // }} /> */}
-            
-            {/* </Router> */}
+            <Router>
+                <UserPanelNav {...props} />
+                <Route path={`/home/:userId/`} render={() => {
+                    return <UserCommunities {...props} />
+                }} />
+
+                <Route path={`/home/:userId/find`} render={() => {
+                    return <UserFindCommunities {...props} />
+                }} />
+            </Router>
         </Col>
     )
 };
